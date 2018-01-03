5904f0ebf1dd465862f44ae204bf494588e95a47
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-03 20:47:49 +0530

    converted model class to room entity

--- app/src/main/java/com/rohan/roomdatabase/User.java
+++ app/src/main/java/com/rohan/roomdatabase/User.java
@@ -1,16 +1,30 @@
 package com.rohan.roomdatabase;
 
+import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
 
 /**
  * Created by rohan on 03/01/18.
  */
 
+@Entity
 public class User {
 
+    @PrimaryKey
     private int userId;
+
+    @ColumnInfo(name = "user_name")
     private String userName;
+
+    @ColumnInfo(name = "user_image")
     private int userImage;
+
+    //If an entity has fields that you don't want to persist, you can annotate them using @Ignore
+    @Ignore
+    private String userFullName;
+
 
     public int getUserId() {
         return userId;
@@ -24,6 +38,10 @@ public class User {
         return userImage;
     }
 
+    public String getUserFullName() {
+        return userFullName;
+    }
+
     public void setUserId(int userId) {
         this.userId = userId;
     }
@@ -34,5 +52,9 @@ public class User {
 
     public void setUserImage(int userImage) {
         this.userImage = userImage;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
     }
 }
