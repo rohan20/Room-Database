28befa1b6042b921ac589b5eb3aa89afa1d2b238
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-05 09:19:57 +0530

    added databinding

--- app/src/main/java/com/rohan/roomdatabase/MainActivity.java
+++ app/src/main/java/com/rohan/roomdatabase/MainActivity.java
@@ -1,15 +1,18 @@
 package com.rohan.roomdatabase;
 
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rohan.roomdatabase.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
