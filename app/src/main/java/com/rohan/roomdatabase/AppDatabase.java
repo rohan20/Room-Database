035b7f172756876ce664bb68887c9d0f546b91bc
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-06 21:15:36 +0530

    updatd database version

--- app/src/main/java/com/rohan/roomdatabase/AppDatabase.java
+++ app/src/main/java/com/rohan/roomdatabase/AppDatabase.java
@@ -7,7 +7,7 @@ import android.arch.persistence.room.RoomDatabase;
  * Created by rohan on 05/01/18.
  */
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao userDao();
 }
